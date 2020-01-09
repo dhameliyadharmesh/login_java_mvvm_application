f77504b41f43b9d93e955f7270662f09b235420a
Dharmesh Dhameliya <dhameliya7dharmesh@gmail.com>
2020-01-09 22:22:27 +0530

    initial commit

---
+++ app/src/main/java/com/sports/games/comjavamvvmapplication/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.sports.games.comjavamvvmapplication.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sports.games.comjavamvvmapplication.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
