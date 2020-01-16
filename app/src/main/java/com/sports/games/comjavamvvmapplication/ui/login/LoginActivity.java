8e76855f555e931245cce4a1f8d485324a308fc1
Dharmesh Dhameliya <dhameliya7dharmesh@gmail.com>
2020-01-16 23:07:52 +0530

    - remove dummy loginviewmodel

--- app/src/main/java/com/sports/games/comjavamvvmapplication/ui/login/LoginActivity.java
+++ app/src/main/java/com/sports/games/comjavamvvmapplication/ui/login/LoginActivity.java
@@ -12,14 +12,14 @@ import com.sports.games.comjavamvvmapplication.utils.EditTextChangeListener;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private LoginViewModelModel loginViewModel;
+    private LoginViewModel loginViewModel;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         LoginActivityBinding loginActivityBinding = DataBindingUtil.setContentView(this, R.layout.login_activity);
-        loginViewModel = ViewModelProviders.of(this).get(LoginViewModelModel.class);
+        loginViewModel = ViewModelProviders.of(this).get(LoginViewModel.class);
         loginActivityBinding.setLifecycleOwner(this);
         loginActivityBinding.setLoginViewModel(loginViewModel);
         loginActivityBinding.edtLoginId.addTextChangedListener(new EditTextChangeListener(loginActivityBinding.tilLoginId));
--- app/src/main/java/com/sports/games/comjavamvvmapplication/ui/login/LoginViewModel.java
+++ app/src/main/java/com/sports/games/comjavamvvmapplication/ui/login/LoginViewModel.java
@@ -1,40 +1,44 @@
 package com.sports.games.comjavamvvmapplication.ui.login;
 
-import androidx.databinding.BaseObservable;
-import androidx.databinding.Bindable;
-import androidx.databinding.library.baseAdapters.BR;
+import android.app.Application;
 
-public class LoginViewModel extends BaseObservable {
+import androidx.databinding.Bindable;
+import androidx.lifecycle.AndroidViewModel;
+
+
+public class LoginViewModel extends AndroidViewModel {
 
     private LoginFormModel loginFormModel;
 
-    public LoginViewModel() {
+    public LoginFormModel getLoginFormModel() {
+        return loginFormModel;
+    }
+
+    public LoginViewModel(Application application) {
+        super(application);
         loginFormModel = new LoginFormModel();
     }
 
     public void setLoginId(String loginId) {
         loginFormModel.setLoginId(loginId);
-        notifyPropertyChanged(BR.loginId);
     }
 
-    public void setLoginPwd(String loginPwd){
+    public void setLoginPwd(String loginPwd) {
         loginFormModel.setLoginPwd(loginPwd);
-        notifyPropertyChanged(BR.loginPwd);
     }
 
-
-    @Bindable
     public String getLoginId() {
         return loginFormModel.getLoginId();
     }
 
-    @Bindable
     public String getLoginPwd() {
         return loginFormModel.getLoginPwd();
     }
 
     public void onSignInClicked() {
+        if (loginFormModel.isValidLoginForm()) {
 
+        }
     }
 
 }
